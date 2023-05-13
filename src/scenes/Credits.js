589ee9bdc756425e0da89386da454dec3edc2377
TonyG <34695086+TonySSBM@users.noreply.github.com>
2023-05-13 06:43:22 -0700

    credits

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -11,10 +11,32 @@ class Credits extends Phaser.Scene{
     }
 
     create(){
-        //
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+              top: 5,
+              bottom: 5,
+            },
+            fixedWidth: 0
+        }
+
+        this.add.text(game.config.width / 2, game.config.height / 2 - borderUISize - borderPadding, 'Credits', menuConfig).setOrigin(0.5);
+        menuConfig.backgroundColor = '#00FF00';
+        menuConfig.color = '#000';
+        this.add.text(game.config.width / 2, game.config.height / 2 + borderUISize + borderPadding, 'Update Later', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 2 + (borderUISize * 2) + borderPadding, 'Update Later', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width / 2, game.config.height / 2 + (borderUISize * 3) + borderPadding, 'Press ← to go back to main menu', menuConfig).setOrigin(0.5);
+
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
     }
 
     update(){
-        //
+        if(Phaser.Input.Keyboard.JustDown(keyLEFT)){
+            this.scene.start('menuScene'); 
+        }
     }
 }
